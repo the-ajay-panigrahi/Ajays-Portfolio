3617086b50cb1ce7bb76688670432a9d8ebb0cac
the-ajay-panigrahi <ajaybpanigrahi@gmail.com>
2024-06-30 15:28:54 +0530

    contact functionality added

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -1,10 +1,10 @@
-import styles from './ContactStyles.module.css';
+import styles from "./ContactStyles.module.css";
 
 function Contact() {
   return (
     <section id="contact" className={styles.container}>
       <h1 className="sectionTitle">Contact</h1>
-      <form action="">
+      <form action="https://formspree.io/f/xyzgyynn" method="post">
         <div className="formGroup">
           <label htmlFor="name" hidden>
             Name
@@ -37,7 +37,8 @@ function Contact() {
             name="message"
             id="message"
             placeholder="Message"
-            required></textarea>
+            required
+          ></textarea>
         </div>
         <input className="hover btn" type="submit" value="Submit" />
       </form>
